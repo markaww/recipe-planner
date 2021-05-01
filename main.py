4ac896b97a4631706405ab96a2a910b3f3966066
markaw <markanthony.westmacott@gmail.com>
2021-05-01 14:10:23 +0200

    readme

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from p_email.m_send import send_email
 
 def main():
     print("running script...")
-    send_email()
+    schedule.every().saturday.at("15:00").do(send_email)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
     print("script complete, check your email!")
 
 if __name__ == '__main__':
